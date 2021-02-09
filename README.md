cc14567318683200daee17e0bd29b199998571da
Saac Sornchai <sornchai.la@ku.th>
2021-02-09 22:20:23 +0700

    🍾 first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# cpp lab00
+
+This is sample project for C++ in github classroom
+
+## The assignment
+
+The function `factorial` is not implemented, fix it to make the main function complete.
+
+## Setup command
+
+N/A
+
+## Run command
+
+`make run`
---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+unsigned long factorial(unsigned long n);
+
+int main() {
+  unsigned long n = 0;
+  cin >> n;
+  cout << factorial(n) << endl;
+}
+
+unsigned long factorial(unsigned long n) {
+  return 0;
+}
